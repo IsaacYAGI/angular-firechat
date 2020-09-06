0a101638137d436c8fe3c95b40751d306c12b8c7
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 16:06:33 -0500

    Refact: Implementa interfaz Mensaje en servicio de chat

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Mensaje } from '../interfaces/mensaje.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  public chats: any[] = [];
-  private itemsCollection: AngularFirestoreCollection<any>;
+  public chats: Mensaje[] = [];
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
   constructor(private afs: AngularFirestore) {
-    // this.itemsCollection = afs.collection<any>('chats');
+    // this.itemsCollection = afs.collection<Mensaje>('chats');
 
   }
 
   cargarMensajes(){
-    this.itemsCollection = this.afs.collection<any>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
 
     return this.itemsCollection.valueChanges();
   }
