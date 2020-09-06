e78ca75d50e8f4bfd131139c3b9d6afd12b5077b
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 16:02:28 -0500

    Feat: Escucha el servicio de chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/providers/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -9,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
 
   mensaje: string = "";
-  constructor() { }
+  constructor(
+    public chatsService: ChatService
+  ) {
+    this.chatsService.cargarMensajes().subscribe( (mensajes: any[]) =>{
+      console.log(mensajes)
+    } );
+   }
 
   ngOnInit(): void {
   }
