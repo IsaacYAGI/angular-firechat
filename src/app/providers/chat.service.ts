bedb3b0c5bbb2a544f0656b7aa2d86bb9b898274
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 16:30:27 -0500

    Feat: Se agregan queries a firebase para traer los ultimos 5 mensajes

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -18,13 +18,16 @@ export class ChatService {
   }
 
   cargarMensajes(){
-    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref
+                                                                        .orderBy('fecha', "desc")
+                                                                        .limit(5));
 
     return this.itemsCollection.valueChanges().pipe(
 
       map( (mensajes: Mensaje[]) =>{
         console.log(mensajes);
-        this.chats = mensajes;
+        this.chats = [];
+        this.chats = mensajes.reverse();
       })
     );
      
