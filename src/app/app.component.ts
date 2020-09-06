dea6e643403d55838f328282a77809eb18572dee
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 17:02:27 -0500

    Refact: Remueve imports y variables no utilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+
+import { ChatService } from './providers/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,9 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'firechat';
 
-  public chats: Observable<any[]>;
   constructor(
-    firestore: AngularFirestore
+    public chatService: ChatService
   ){
-    this.chats = firestore.collection('chats').valueChanges();
+    
   }
 }
