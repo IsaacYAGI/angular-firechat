d0de356bd6f644806640bd1b783214520b92fd23
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 16:19:31 -0500

    Feat: Agrega metodo para guardar mensaje

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Mensaje } from '../interfaces/mensaje.interface';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,6 +20,24 @@ export class ChatService {
   cargarMensajes(){
     this.itemsCollection = this.afs.collection<Mensaje>('chats');
 
-    return this.itemsCollection.valueChanges();
+    return this.itemsCollection.valueChanges().pipe(
+
+      map( (mensajes: Mensaje[]) =>{
+        console.log(mensajes);
+        this.chats = mensajes;
+      })
+    );
+     
+  }
+
+  agregarMensaje( texto: string ){
+    //falta uid del usuario
+    let mensaje: Mensaje = {
+      nombre: "Demo",
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add(mensaje);
   }
 }
