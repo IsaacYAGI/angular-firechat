402dede9013166ac7f534acbea7114f2e3ddcfbd
Isaac Yriarte <isaacyagi@gmail.com>
2020-09-06 16:02:17 -0500

    Feat: Agrega servicio de chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { ChatComponent } from './components/chat/chat.component';
+import { ChatService } from './providers/chat.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { ChatComponent } from './components/chat/chat.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
